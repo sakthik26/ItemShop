620372ae122312be272357585810f63045b561a5
sakthi kumar <rsakthikumar26@gmail.com>
2020-03-28 14:11:18 +0100

    Variant Selection changes involving product sizes

--- client/components/carousel/Carousel.vue
+++ client/components/carousel/Carousel.vue
@@ -2,8 +2,6 @@
   <v-carousel
     height="400"
     hide-delimiter-background
-    show-arrows-on-hover
-
   >
     <v-carousel-item
       v-for="(slide, i) in slides"
@@ -30,15 +28,7 @@ export default {
   name: "Carousel",
   props: ["slides"],
   data() {
-    return {
-      // slides: [
-      //   "https://images.unsplash.com/photo-1453831362806-3d5577f014a4?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
-      //   "https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
-      //   "https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
-      //   "https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ",
-      //   "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-      // ]
-    };
+    return {};
   }
 };
 </script>
