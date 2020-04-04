5d15eeb5d6e9f3387ac3b30a523e94d7ecfe3236
sakthi kumar <rsakthikumar26@gmail.com>
2020-04-04 19:49:48 +0200

    Temp

---
+++ client/plugins/localStorage.js
@@ -0,0 +1,25 @@
+import createPersistedState from 'vuex-persistedstate';
+import SecureLS from "secure-ls";
+var ls = new SecureLS({ isCompression: false });
+
+// let cookieStorage = {
+//   getItem: function (key) {
+//     return Cookies.getJSON(key);
+//   },
+//   setItem: function (key, value) {
+//     return Cookies.set(key, value, { expires: 3, secure: false });
+//   },
+//   removeItem: function (key) {
+//     return Cookies.remove(key);
+//   }
+// };
+
+export default (context) => {
+  createPersistedState({
+    storage: {
+      getItem: (key) => ls.get(key),
+      setItem: (key, value) => ls.set(key, value),
+      removeItem: (key) => ls.remove(key),
+    },
+  })(context.store);
+};
